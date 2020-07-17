66c7599fcb87c8be1057a471062504dffd4d297d
Alex Unku <catwalkghost@gmail.com>
2020-07-17 17:19:26 +0100

    fetching data from Firebase

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 import IngredientForm from './IngredientForm'
 import IngredientList from './IngredientList'
@@ -11,13 +11,38 @@ const Ingredients = (props) => {
 
     const [userIngredients, setUserIngredients] = useState([]) // array is used because it's a list of ings
 
-    const addIngredientHandler = (ingredient) => {
+    // HTTP-requests are typical side-effects
+    // Gets executed right AFTER the (and for EVERY) render cycle
+    useEffect(() => {
+        // get requests are executed by default. I.e. no need to specify method
+        fetch(c.DB_URL + 'ingredients.json' )
+            .then(resp => {
+                return resp.json()
+            })
+            .then(respData => {
+                // Firebase returns an object, that needs to be converted into an array:
+                let loadedIngredients = []
+                for (let key in  respData) {
+                    loadedIngredients.push({
+                        id: key,
+                        title: respData[key].title,
+                        amount: respData[key].amount,
+                    })
+                }
+                // After the conversion is done, setUserIngredients is called using our new array to populate the state
+                setUserIngredients(loadedIngredients)
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    },[])
 
+    const addIngredientHandler = (ingredient) => {
         // body is JSON data
         // No id, because Firebase will generate it
         fetch(c.DB_URL + 'ingredients.json', {
             method: 'post',
-            body: JSON.stringify({ ingredient}),
+            body: JSON.stringify(ingredient),
             headers: { 'Content-Type': 'application/json' }
         })
             .then(resp => {
