6a56565dab8b6f0261b042c7237b8c1a147b2c05
Alex Unku <catwalkghost@gmail.com>
2020-07-17 16:59:28 +0100

    posting data to Firebase

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -3,6 +3,8 @@ import React, { useState } from 'react'
 import IngredientForm from './IngredientForm'
 import IngredientList from './IngredientList'
 
+import * as c from '../../shared/const'
+
 import Search from './Search'
 
 const Ingredients = (props) => {
@@ -10,15 +12,37 @@ const Ingredients = (props) => {
     const [userIngredients, setUserIngredients] = useState([]) // array is used because it's a list of ings
 
     const addIngredientHandler = (ingredient) => {
-        setUserIngredients(prevIngredients => [
-            ...prevIngredients,
-            {
-                id: Math.random().toString(),
-                // Spread operator takes ALL key–value pairs from an old obj and adds them
-                // as key–value pairs to the new object
-                ...ingredient,
-            },
-        ])
+
+        // body is JSON data
+        // No id, because Firebase will generate it
+        fetch(c.DB_URL + 'ingredients.json', {
+            method: 'post',
+            body: JSON.stringify({ ingredient}),
+            headers: { 'Content-Type': 'application/json' }
+        })
+            .then(resp => {
+                return resp.json()
+            })
+            .then(respData => {
+                const { name } = respData
+                setUserIngredients(prevIngredients => [
+                    ...prevIngredients,
+                    { id: name, ...ingredient }
+                ])
+            })
+            .catch(err => {
+                console.log(err)
+            })
+
+        // setUserIngredients(prevIngredients => [
+        //     ...prevIngredients,
+        //     {
+        //         id: Math.random().toString(),
+        //         // Spread operator takes ALL key–value pairs from an old obj and adds them
+        //         // as key–value pairs to the new object
+        //         ...ingredient,
+        //     },
+        // ])
     }
 
     return (
---
+++ src/shared/const.js
@@ -0,0 +1,1 @@
+export const DB_URL = 'https://react-hooks-b1b9f.firebaseio.com/'
