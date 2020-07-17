7926e4ff1cc84680b9f067c5e3a50e326bf1fa18
Alex Unku <catwalkghost@gmail.com>
2020-07-17 13:27:44 +0100

    adding basic usage of setState

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -1,28 +1,82 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import Card from '../UI/Card'
 import './IngredientForm.css'
 
 const IngredientForm = React.memo(props => {
+
+    /* --------------------------------------------------
+    Use state can be initialised with a default state:
+    It can be an object, value, etc.
+    This is an important difference from 'normal' state
+    In class-based components, state HAS to be an object
+    -------------------------------------------------- */
+
+    // useState always returns an array with two elements:
+    // current state snapshot, and a function to update the currnet state
+
+    // const inputState =
+    //     useState({
+    //         title: '',
+    //         amount: '',
+    //     })
+
+    // Array destructuring
+    const [ inputState, setInputState] = useState({ title: '', amount: '' })
+
+
+
     const submitHandler = event => {
         event.preventDefault()
         // ...
     }
 
+    // First element of the array is the snapshot of the current state
+    // const { title, amount } = inputState[0]
+    const { title, amount } = inputState
+
     return (
-        <section className="ingredient-form">
+        <section className='ingredient-form'>
             <Card>
                 <form onSubmit={submitHandler}>
-                    <div className="form-control">
-                        <label htmlFor="title">Name</label>
-                        <input type="text" id="title" />
+                    <div className='form-control'>
+                        <label htmlFor='title'>Name</label>
+                        <input
+                            type='text'
+                            id='title'
+                            value={title}
+                            // Hooks DON'T merge the state, but 're-create' it !!!
+                            // prevInputState allows us to access previous input state
+                            onChange={event => {
+                                // creating a const, will help to bypass the closure:
+                                // React won't save and re-use the old event
+                                const newTitle = event.target.value
+                                // inputState[1](prevInputState => ({
+                                setInputState(prevInputState => ({
+                                title: newTitle,
+                                amount: prevInputState.amount,
+                            }))}} />
                     </div>
-                    <div className="form-control">
-                        <label htmlFor="amount">Amount</label>
-                        <input type="number" id="amount" />
+                    <div className='form-control'>
+                        <label htmlFor='amount'>Amount</label>
+                        <input
+                            type='number'
+                            id='amount'
+                            value={amount}
+                            onChange={event => {
+                                // creating a const, will help to bypass the closure:
+                                // React won't save and re-use the old event
+                                // Closure will happen on the new expression
+                                const newAmount = event.target.value
+                                // inputState[1](prevInputState => ({
+                                setInputState(prevInputState => ({
+                                    title: prevInputState.title,
+                                    amount: newAmount,
+                                }))
+                            }} />
                     </div>
-                    <div className="ingredient-form__actions">
-                        <button type="submit">Add Ingredient</button>
+                    <div className='ingredient-form__actions'>
+                        <button type='submit'>Add Ingredient</button>
                     </div>
                 </form>
             </Card>
--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -5,11 +5,11 @@ import './Search.css'
 
 const Search = React.memo(props => {
     return (
-        <section className="search">
+        <section className='search'>
             <Card>
-                <div className="search-input">
+                <div className='search-input'>
                     <label>Filter by Title</label>
-                    <input type="text" />
+                    <input type='text' />
                 </div>
             </Card>
         </section>
